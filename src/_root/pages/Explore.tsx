ecb721942a9f9fdd9ea43c6f13097376412296a2
UditRawall <uditrawal209@gmail.com>
2024-05-15 22:33:21 +0530

    done

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,9 +1,30 @@
-// import GridPostList from "@/components/shared/GridPostList";
+
+
 // import SearchResults from "@/components/shared/SearchResults";
 import { Input } from "@/components/ui/input";
+// import { useInView } from '/'
+
 import { useState } from "react";
 
+export type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: any;
+};
+
+// const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+//   if (isSearchFetching) {
+//     return <Loader />;
+//   } else if (searchedPosts && searchedPosts.documents.length > 0) {
+//     return <GridPostList posts={searchedPosts.documents} />;
+//   } else {
+//     return (
+//       <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+//     );
+//   }
+// };
+
 const Explore = () => {
+  // const { ref, inView } = useInView();
   const [searchValue, setSearchValue] = useState("");
 
   
@@ -42,7 +63,8 @@ const Explore = () => {
       </div>
       {/* <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
-          <SearchResults />
+          // <SearchResults />
+          <p>hi</p>
         ) : shouldShowPosts ? (<p className="text-light-4 mt-10 text-center w-full">End of posts</p>): PostStats.pages.map((item, index)=>(
           <GridPostList key={`page-${index}`} posts= {item.documnets} />
         ))
